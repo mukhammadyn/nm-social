b96168a34123335ff81a95fabf30b89abc6244a5
mukhammadyn <nurmuhammaddeveloper@gmail.com>
2022-03-23 10:18:36 +0500

    add color selector

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 const elDevArrow = document.querySelector('.dev')
 const elModeBtn = document.querySelector('.js-mode-btn')
-
-const btnDesc = {
-  dark: 'Click me)',
-  light: 'Thank you)'
-}
+const elThemeColor = document.getElementById('color')
 
 function setAnimate() {
   elDevArrow.classList.toggle('animation')
+}
+
+function onThemeChange() {
+  let shadowColor = elThemeColor.value.split('')
+  shadowColor = shadowColor.map((color, i) => i % 2 ? color = '5' : color = color)
+  shadowColor = shadowColor.join('')
+
+  document.documentElement.style.setProperty('--light-color', elThemeColor.value)
+  document.documentElement.style.setProperty('--dark-color', elThemeColor.value)
+  document.documentElement.style.setProperty('--dark-shadow', shadowColor)
+  document.documentElement.style.setProperty('--light-shadow', shadowColor)
+  document.documentElement.style.setProperty('--lighter-shadow', shadowColor)
 }
 
 if(elDevArrow) {
@@ -19,10 +27,9 @@ if(elDevArrow) {
 if(elModeBtn) {
   elModeBtn.addEventListener('click', () => {
     document.body.classList.toggle('page__body--light')
-    if(document.body.matches('.page__body--light')) {
-      elModeBtn.textContent = btnDesc.light
-    } else {
-      elModeBtn.textContent = btnDesc.dark
-    }
   })
+}
+
+if(elThemeColor) {
+  elThemeColor.addEventListener('change', onThemeChange)
 }
