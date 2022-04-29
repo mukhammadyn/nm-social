16201e699d6f7e0f2a75685d75196a085faa0307
mukhammadyn <nurmuhammaddeveloper@gmail.com>
2022-04-30 01:09:37 +0500

    change tg link and comment js code

--- js/main.js
+++ js/main.js
@@ -6,18 +6,6 @@ function setAnimate() {
   elDevArrow.classList.toggle('animation')
 }
 
-function onThemeChange() {
-  let shadowColor = elThemeColor.value.split('')
-  shadowColor = shadowColor.map((color, i) => i % 2 ? color = '5' : color = color)
-  shadowColor = shadowColor.join('')
-
-  document.documentElement.style.setProperty('--light-color', elThemeColor.value)
-  document.documentElement.style.setProperty('--dark-color', elThemeColor.value)
-  document.documentElement.style.setProperty('--dark-shadow', shadowColor)
-  document.documentElement.style.setProperty('--light-shadow', shadowColor)
-  document.documentElement.style.setProperty('--lighter-shadow', shadowColor)
-}
-
 if(elDevArrow) {
   setInterval(() => {
     setAnimate()
